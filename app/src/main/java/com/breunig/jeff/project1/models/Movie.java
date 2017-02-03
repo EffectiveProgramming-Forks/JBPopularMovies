bf258c2575e1640fc52b2e9c20cb893869b1e540
Jeff Breunig <jkbreunig@gmail.com>
2017-02-02 20:17:46 -0600

    update parsing movies json

--- app/src/main/java/com/breunig/jeff/project1/models/Movie.java
+++ app/src/main/java/com/breunig/jeff/project1/models/Movie.java
@@ -1,5 +1,8 @@
 package com.breunig.jeff.project1.models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by jkbreunig on 2/2/17.
  */
@@ -10,5 +13,12 @@ public class Movie {
     public String mReleaseDate;
     public String mVoteAverage;
     public String mPosterPath;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        this.mTitle = jsonObject.getString("title");
+        this.mOverview = jsonObject.getString("overview");
+        this.mReleaseDate = jsonObject.getString("release_date");
+        this.mVoteAverage = jsonObject.getString("vote_average");
+    }
 
 }
--- app/src/main/java/com/breunig/jeff/project1/utilities/MovieJsonUtils.java
+++ app/src/main/java/com/breunig/jeff/project1/utilities/MovieJsonUtils.java
@@ -20,14 +20,12 @@ public final class MovieJsonUtils {
     public static Movie[] getMoviesFromJson(Context context, String jsonStr)
             throws JSONException {
 
-        final String OWM_MESSAGE_CODE = "cod";
-
         Movie[] movies = null;
 
         JSONObject moviesJson = new JSONObject(jsonStr);
 
-        if (moviesJson.has(OWM_MESSAGE_CODE)) {
-            int errorCode = moviesJson.getInt(OWM_MESSAGE_CODE);
+        if (moviesJson.has("status_code")) {
+            int errorCode = moviesJson.getInt("status_code");
 
             switch (errorCode) {
                 case HttpURLConnection.HTTP_OK:
@@ -47,26 +45,11 @@ public final class MovieJsonUtils {
         for (int i = 0; i < moviesJsonArray.length(); i++) {
             String date;
             String highAndLow;
-            Movie movie = new Movie();
             JSONObject movieJsonObject = moviesJsonArray.getJSONObject(i);
-            movie.mTitle = movieJsonObject.getString("title");
-
+            Movie movie = new Movie(movieJsonObject);
             movies[i] = movie;
         }
 
         return movies;
-    }
-
-    /**
-     * Parse the JSON and convert it into ContentValues that can be inserted into our database.
-     *
-     * @param context         An application context, such as a service or activity context.
-     * @param forecastJsonStr The JSON to parse into ContentValues.
-     *
-     * @return An array of ContentValues parsed from the JSON.
-     */
-    public static ContentValues[] getFullWeatherDataFromJson(Context context, String forecastJsonStr) {
-        /** This will be implemented in a future lesson **/
-        return null;
     }
 }
