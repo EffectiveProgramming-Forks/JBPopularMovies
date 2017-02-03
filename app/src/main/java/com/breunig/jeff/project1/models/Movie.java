79a4d15dd3a7b56c5e8c43cb43aa4aead8a31a24
Jeff Breunig <jkbreunig@gmail.com>
2017-02-03 14:52:26 -0600

    update original title

--- app/src/main/java/com/breunig/jeff/project1/models/Movie.java
+++ app/src/main/java/com/breunig/jeff/project1/models/Movie.java
@@ -17,7 +17,7 @@ public class Movie implements Serializable {
     public String posterPath;
 
     public Movie(JSONObject jsonObject) throws JSONException {
-        this.title = jsonObject.getString("title");
+        this.title = jsonObject.getString("original_title");
         this.overview = jsonObject.getString("overview");
         this.releaseDate = jsonObject.getString("release_date");
         this.userRating = jsonObject.getString("vote_average");
